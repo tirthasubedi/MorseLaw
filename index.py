3511bbdbc42d0eb9dd1f89779063abb544a96941
[tirthasubedi] <[subedi.tirtha@gmail.com]>
2019-11-21 00:03:37 -0500

    adding all the files

---
+++ index.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from main import search, convertToMorseCode
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'stuff'
+
+
+class UrlLink(FlaskForm):
+    searchbox = StringField("Search Term:", validators=[DataRequired()])
+    submit = SubmitField('Search')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def divide():
+    form = UrlLink()
+    morseCode = None
+    if form.validate_on_submit():
+        text = form.searchbox.data
+        law = search(text)
+        morseCode = convertToMorseCode(law)
+
+    else:
+        print("not here")
+
+    return render_template('index.html', form=form, morsecode=morseCode)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ main.py
@@ -0,0 +1,79 @@
+import requests
+
+import textract
+from collections import defaultdict
+from os import getcwd
+from os.path import join
+
+
+def convertToMorseCode(text):
+    # clean it, convert it, return it
+    MORSE_CODE_DICT = defaultdict(str)
+    MORSE_CODE_DICT.update({'A': '.-', 'B': '-...',
+                            'C': '-.-.', 'D': '-..', 'E': '.',
+                            'F': '..-.', 'G': '--.', 'H': '....',
+                            'I': '..', 'J': '.---', 'K': '-.-',
+                            'L': '.-..', 'M': '--', 'N': '-.',
+                            'O': '---', 'P': '.--.', 'Q': '--.-',
+                            'R': '.-.', 'S': '...', 'T': '-',
+                            'U': '..-', 'V': '...-', 'W': '.--',
+                            'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                            '1': '.----', '2': '..---', '3': '...--',
+                            '4': '....-', '5': '.....', '6': '-....',
+                            '7': '--...', '8': '---..', '9': '----.',
+                            '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                            '?': '..--..', '/': '-..-.', '-': '-....-',
+                            '(': '-.--.', ')': '-.--.-'})
+    text = text.upper()
+    new_text = []
+    for char in text:
+        new_text.append(MORSE_CODE_DICT[char])
+    return ' '.join(new_text)
+
+
+def search(phrase):
+    cookies = {
+        'ASP.NET_SessionId': 'ervja3vcyqg51q503aewmm1d',
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'RequestVerificationToken': 'CUXTJLtT-FuTCeb8F_UfKGkn_4wH6MY5Ox6LBPR-a4WWB7s42cN7Se4X5-_5A7m98AtppitxWH58-HtRzSz_08PQPNZJ6-JZGzcWuL9SFUujZQAxe2R_YnD9A4vT3dCCi49Gxc37JjHsuTDS5JSg0Q2:CMp9KAFlIfFYoSScfKPzQwHxJm2ERi7uCRL3n48OsR-xQiw2PznWxCt504SnS1ttZxZRjs_X3uLJJrapB_slqi0JtGftCt4Z3BjdrIJJAhhJTjPDfR2I04GvfuP8latwmXPlPibLR8lxNiAGI1QPPtr55LdwFtm-qfNWRAoaStg1',
+        'X-Requested-With': 'XMLHttpRequest',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Referer': 'https://apps.legislature.ky.gov/lrcsearch',
+        'TE': 'Trailers',
+    }
+
+    params = (
+        ('searchPhrase', phrase),
+        ('statuteRange', 'on'),
+        ('startRange', '1'),
+        ('endRange', '999'),
+        ('X-Requested-With', 'XMLHttpRequest'),
+    )
+
+    response = requests.get('https://apps.legislature.ky.gov/LRCSearch/Home/getStatuteJSON', headers=headers,
+                            params=params, cookies=cookies)
+    if len(response.json()) == 0:
+        text = 'no results found'
+    else:
+        pdf_response = requests.get(response.json()[0]["RSN"])
+        with open('results.pdf', 'wb') as fileobj:
+            fileobj.write(pdf_response.content)
+        text = textract.process(join(getcwd(), 'results.pdf'))
+        text = text.decode("utf-8")
+    return text
+
+
+def main():
+    search_phrase = "funeral interference"
+    text = search(search_phrase)
+    print(text)
+
+
+if __name__ == '__main__':
+    main()
